994d0997e23aca4511df439fb273307191376f16
abhisanGhimire <abhisanghimire@dr-ait.org>
2020-11-07 14:04:37 +0545

    lekhey

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -18,5 +18,6 @@ class TestController extends Controller
         //dd($decoded_info); //To get all info uncomment the line
         //dump and die the number of positive cases
         dd($decoded_info->tested_positive);
+        dd();//Lekhey
     }
 }
